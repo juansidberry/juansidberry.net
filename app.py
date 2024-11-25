31dde59bcb81af560174d53bf060244961fd9008
Juan Sidberry <juan.sidberry@insightglobal.com>
2024-11-25 17:55:13 -0500

    chg website name

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def home():
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Basic Web Application</title>
+    <title>Juan Sidberry</title>
     <style>
         body {
             background-color: #eeeeee;
